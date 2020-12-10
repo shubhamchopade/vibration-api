667dd03a5d735ba4153ceb641460ebb086f36de6
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2020-12-11 00:55:41 +0530

    second

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const btn = document.querySelector(".vibration__button");
 
-btn.addEventListener("click", () => {
-  navigator.vibrate([500]);
-});
+btn.addEventListener("click", vibrateOnce);
+
+function vibrateOnce() {
+  alert("Hello");
+  navigator.vibrate(500);
+}
